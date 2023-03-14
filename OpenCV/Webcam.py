32bf18af2f836c8674ff67981f7ddf11c1fc33c5
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2023-03-14 13:54:35 +0000

    Commit opencv webcam code

---
+++ OpenCV/Webcam.py
@@ -0,0 +1,13 @@
+#pip install opencv-python
+import cv2
+
+#=========================================================
+# # Подключение к вэбкамере
+# cap = cv2.VideoCapture(0)
+# while True:
+#     _, frame = cap.read()
+#     cv2.inshow("camera", frame)
+
+#     if cv2.waitKey(1) & 0xff == ord('q'):
+#         break
+#=========================================================
