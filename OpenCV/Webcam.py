0a477fe64bad547defcab760522fa11844cf97df
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2023-03-17 07:16:33 +0000

    Modified code opencv

--- OpenCV/Webcam.py
+++ OpenCV/Webcam.py
@@ -35,19 +35,19 @@ import cv2
 #=====================================================================
 # Распознание через вэбкамеру лица и выделение его в прямоугольник
 
-face_cascades = cv2.CascadeClassifier(cv2.data.hearcascades + "hearcascade_frontalface_default.xml")
-cap = cv2.VideoCapture(0)
+# face_cascades = cv2.CascadeClassifier(cv2.data.hearcascades + "hearcascade_frontalface_default.xml")
+# cap = cv2.VideoCapture(0)
 
-while True:
-    _, frame = cap.read()
-    img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = face_cascades.detectMultiScale(img_gray)
+# while True:
+#     _, frame = cap.read()
+#     img_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     faces = face_cascades.detectMultiScale(img_gray)
 
-    for(x, y, w, h) in faces:
-        cv2.rectangle(img,(x,y), (x + w, y + h), (255, 0, 0), 2)
+#     for(x, y, w, h) in faces:
+#         cv2.rectangle(img,(x,y), (x + w, y + h), (255, 0, 0), 2)
     
-    cv2.imshow('Result', frame)
+#     cv2.imshow('Result', frame)
 
-    if cv2.waitKey(1) & 0xff == ord('q')
-        break
+#     if cv2.waitKey(1) & 0xff == ord('q'):
+#         break
 #=====================================================================
