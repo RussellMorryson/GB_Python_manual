850a07bfa9d9a6dcca44ca1d5155b8d9ee7e0d79
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2023-03-21 10:14:52 +0000

    Complete task 27

--- Seminar_4/Task_27.py
+++ Seminar_4/Task_27.py
@@ -3,12 +3,29 @@
 подряд, слова разделены одним или большим числом
 пробелов. Определите, сколько различных слов
 содержится в этом тексте.
-Input: She sells sea shells on the sea shore The shells
-that she sells are sea shells I'm sure.So if she sells sea
-shells on the sea shore I'm sure that the shells are sea
-shore shells
+Input: She sells sea shells on the sea shore. The shells that she sells are sea shells. I'm sure. So if she sells sea shells on the sea shore. I'm sure that the shells are sea shore shells.
 Output: 13
 """
-
+print("Введите текст: ")
 text = str(input())
+text = text.lower()
+word = ''
+words = []
 
+for i in text:
+    if i == '.':
+        words.append(word)
+    elif i != ' ':
+        word = word + i    
+    else:
+        words.append(word)
+        word = ''
+
+print(words)
+words_number = {'0'}
+
+for temp in words:
+    words_number.add(temp)
+words_number.remove('0')
+
+print(len(words_number))
