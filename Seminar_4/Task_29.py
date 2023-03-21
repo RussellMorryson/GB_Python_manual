ef11b7b83f50102d2c6e1af8956b67043d0af2f5
RussellMorryson <118932212+RussellMorryson@users.noreply.github.com>
2023-03-21 10:29:52 +0000

    Add code task 29

--- Seminar_4/Task_29.py
+++ Seminar_4/Task_29.py
@@ -13,3 +13,21 @@
 
 За помощью товарищи обратились к Вам, студентам.
 Примечание: Программные коды на следующих слайдах"""
+
+#vano
+n = int(input())
+max_number = 1000
+while n != 0:
+    n = int(input())
+    if max_number > n:
+        max_number = n
+print(max_number)
+
+# petr
+n = int(input())
+max_number = -1
+while n < 0:
+    n = int(input())
+    if max_number < n:
+        n = max_number
+print(n) 
